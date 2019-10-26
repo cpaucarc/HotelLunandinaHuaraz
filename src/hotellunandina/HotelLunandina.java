3076ac1e95bbe3aaa80cc04c11e5459060c6b2d6
Cesar_W <PAUCAR@DESKTOP-OGL3Q4L>
2019-10-26 16:57:52 -0500

    primera version, se desarrollo MenuPrincipal

---
+++ src/hotellunandina/HotelLunandina.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotellunandina;
+
+import Ventanas.*;
+/**
+ *
+ * @author PAUCAR
+ */
+public class HotelLunandina {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MenuPrincipal mp = new MenuPrincipal();
+        mp.setVisible(true);
+    }
+    
+}
